a299710536ec8d378daefc477c9d1a85521087d8
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:54:13 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "CHIME_Crop_Health_and_Yield_Intelligence_Engine" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
